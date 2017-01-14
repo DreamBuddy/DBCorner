dc33e06977f01db7e6d52e41071d848a030f322d
Xu Mengtong <xumengtong@imakejoy.com>
2017-01-15 00:35:17 +0800

    fix error when compute the border length.修复描边计算精确度引起的描边显示问题.

--- Class/UIView+DBCorner.m
+++ Class/UIView+DBCorner.m
@@ -10,28 +10,9 @@
 
 static NSString *DBCornerLayerName = @"DBCornerShapeLayer";
 #define DB_SINGLE_LINE_WIDTH (1 / [UIScreen mainScreen].scale)
+#define DB_Screen_Scale ([UIScreen mainScreen].scale)
 
 @implementation UIView (DBCorner)
-
-/**
- 关于 CGPath 的 length 的计算请参看 http://www.mlsite.net/blog/?p=1312 与 http://stackoverflow.com/questions/6515158/get-info-about-a-cgpath-uibezierpath 在这里简单的计算就能满足要求因此不做过多讨论
- */
-float lengthOfCGPath (UIRectCorner corner,CGFloat radius,CGSize size) {
-    CGFloat totolLength = 2*(size.width + size.height);
-    if (corner & UIRectCornerTopLeft) {
-        totolLength -= (2*radius - (M_PI * radius)/2);
-    }
-    if (corner & UIRectCornerTopRight) {
-        totolLength -= (2*radius - (M_PI * radius)/2);
-    }
-    if (corner & UIRectCornerBottomLeft) {
-        totolLength -= (2*radius - (M_PI * radius)/2);
-    }
-    if (corner & UIRectCornerBottomRight) {
-        totolLength -= (2*radius - (M_PI * radius)/2);
-    }
-    return totolLength;
-}
 
 - (void)db_roundingCornerWithRadius:(CGFloat)radius backgroundColor:(UIColor *)bgColor borderColor:(UIColor *)borderColor{
     [self db_roundingCorner:UIRectCornerAllCorners radius:radius backgroundColor:bgColor borderColor:borderColor borderWidth:DB_SINGLE_LINE_WIDTH rect:CGRectNull];
@@ -43,7 +24,8 @@ float lengthOfCGPath (UIRectCorner corner,CGFloat radius,CGSize size) {
 
 - (void)db_roundingCorner:(UIRectCorner)roundCorner radius:(CGFloat)radius backgroundColor:(UIColor *)bgColor borderColor:(UIColor *)borderColor borderWidth:(CGFloat )borderWidth rect:(CGRect)realRect{
     CGRect bounds = CGSizeEqualToSize(realRect.size, CGSizeZero) ? self.bounds : realRect;
-    
+    bounds.size.width += .13*DB_Screen_Scale;
+    bounds.size.height += .13*DB_Screen_Scale;
     /*
      需要 优化 AutoLayout 自动计算
      CGSize size = [customContentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize];
@@ -53,11 +35,13 @@ float lengthOfCGPath (UIRectCorner corner,CGFloat radius,CGSize size) {
     
     CGFloat width = CGRectGetWidth(bounds);
     CGFloat height = CGRectGetHeight(bounds);
-    UIBezierPath * path= [UIBezierPath bezierPathWithRect:CGRectMake(0, 0, width, height)];
+    UIBezierPath * path= [UIBezierPath bezierPathWithRect:CGRectMake(-0.2, -0.2, width, height)];
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
     shapeLayer.name = DBCornerLayerName;
-    UIRectCorner sysCorner = (UIRectCorner)roundCorner;
-    UIBezierPath *cornerPath = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:sysCorner cornerRadii:CGSizeMake(radius, radius)];
+    /*
+     http://huanyueyaoqin.com/2015/07/15/UIBezierPath%E5%AD%A6%E4%B9%A0%E6%80%BB%E7%BB%93/
+     */
+    UIBezierPath *cornerPath = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:roundCorner cornerRadii:CGSizeMake(radius, 0)];
     [path  appendPath:cornerPath];
     //[path setUsesEvenOddFillRule:YES];
     shapeLayer.path = path.CGPath;
@@ -67,13 +51,13 @@ float lengthOfCGPath (UIRectCorner corner,CGFloat radius,CGSize size) {
     shapeLayer.fillRule = kCAFillRuleEvenOdd;
     shapeLayer.fillColor = bgColor.CGColor;
     if (borderColor) {
-        //CGPathApply
-        CGFloat cornerPathLength = lengthOfCGPath(roundCorner,radius,bounds.size);
-        CGFloat totolPathLength = 2*(CGRectGetHeight(bounds)+CGRectGetWidth(bounds))+cornerPathLength;
-        shapeLayer.strokeStart = (totolPathLength-cornerPathLength)/totolPathLength;
-        shapeLayer.strokeEnd = 1.0;
-        shapeLayer.strokeColor = borderColor.CGColor;
-        shapeLayer.lineWidth = borderWidth;
+        UIBezierPath *borderPath = [UIBezierPath bezierPathWithRoundedRect:bounds byRoundingCorners:roundCorner cornerRadii:CGSizeMake(radius, 0)];
+        CAShapeLayer *borderLayer = [CAShapeLayer layer];
+        borderLayer.path = borderPath.CGPath;
+        borderLayer.strokeColor = borderColor.CGColor;
+        borderLayer.lineWidth = borderWidth;
+        borderLayer.fillColor = UIColor.clearColor.CGColor;
+        [shapeLayer addSublayer:borderLayer];
     }
     if ([self isKindOfClass:[UILabel class]]) {
         //UILabel 机制不一样的  UILabel 设置 text 为 中文 也会造成图层混合 (iOS8 之后UILabel的layer层改成了 _UILabelLayer 具体可阅读 http://www.jianshu.com/p/db6602413fa3 )
